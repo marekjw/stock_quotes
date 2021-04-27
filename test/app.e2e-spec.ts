817dbea8ff49423e6bd4c236a0b8116ddc1e724b
marek <marekjwesolowski@gmail.com>
2021-04-28 00:31:33 +0200

    Added concurent tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -78,4 +78,15 @@ describe('AppController (e2e)', () => {
       })
       .expect(400)
   })
+
+  test.concurrent.each(Array.from(Array(100).keys()))('POSTing new record %d', (i) => {
+    return request("localhost:8000")
+      .post('/quotes')
+      .send({
+        timestamp: i,
+        price: 100,
+        ticker: 'abc' + Math.floor(i / 10),
+      })
+      .expect(201)
+  })
 });
